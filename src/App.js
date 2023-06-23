5c75bc4c077cf5892e6c6b897d47b892edd9fbc2
joaoCS <joaoantoniogba@hotmail.com>
2023-06-23 08:18:55 -0300

    feat: responsive grid

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
-import './App.css';
+import { useEffect, useState } from "react"
+import "./App.css"
+import { api } from "./utils/api"
 
 function App() {
-  return (
-    <div className="App">
-        Olá
-    </div>
-  );
+    const [games, setGames] = useState([])
+
+    useEffect(() => {
+        async function fetchGames() {
+            const response = await api.get("/data", {
+                headers: {
+                    "dev-email-address": "joaoantoniogba025@gmail.com",
+                },
+            })
+
+            console.log(response)
+            setGames(response.data)
+        }
+
+        fetchGames()
+    }, [])
+
+    return (<div className="App">
+        <ul>
+          {games.map(game => {
+            return (
+              <li key={game.id}>
+                <img src={game.thumbnail}/>
+                <h3>{game.title}</h3>
+              </li>
+            )
+          })}
+        </ul>
+      Olá</div>)
 }
 
-export default App;
+export default App
